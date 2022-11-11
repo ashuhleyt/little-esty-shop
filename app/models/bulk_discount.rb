980f21f8be87d445375341c16a24f38e7009f7cb
Ashley <ashuhleyt@gmail.com>
2022-11-11 14:45:16 -0800

    fixed relationships

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,4 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant 
-  has_many :invoice_items 
+  
+
+  validates :discount, presence: true, numericality: true
+  validates :threshold, presence: true, numericality: { only_integer: true }
 end
