3db2f299b96cf60571834c75916ea81ebddb8251
Ashley <ashuhleyt@gmail.com>
2022-11-11 18:17:32 -0800

    link to edit discount on show page

--- spec/features/bulk_discount/show_spec.rb
+++ spec/features/bulk_discount/show_spec.rb
@@ -9,14 +9,18 @@ RSpec.describe 'Bulk Discount Show Page' do
     @discount2 = BulkDiscount.create!(discount: 30, threshold: 15, merchant_id: @merchant_1.id)
     @discount3 = BulkDiscount.create!(discount: 50, threshold: 25, merchant_id: @merchant_2.id)
     @discount4 = BulkDiscount.create!(discount: 10, threshold: 3, merchant_id: @merchant_2.id)
+    visit merchant_bulk_discount_path(@merchant_1, @discount1)
   end
 
   describe 'bulk discount show' do 
     it 'I see the bulk discount threshold and percentage discount' do 
-      visit merchant_bulk_discount_path(@merchant_1, @discount1)
-save_and_open_page
       expect(page).to have_content(@discount1.discount)
       expect(page).to have_content(@discount1.threshold)
+      save_and_open_page
+    end
+
+    it 'I see a link to edit the bulk discount' do 
+      expect(page).to have_link("Edit #{@discount1.id}")
     end
   end
 end
